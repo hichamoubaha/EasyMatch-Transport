a6a7aca9077e013c9cfb3d9e4b3521c0af29ecf6
Abderrahmane_Ahouari <abdorrahmane054@gmail.com>
2025-02-11 15:08:08 +0100

    la base de donner est créer

--- v2.sql
+++ v2.sql
@@ -4,17 +4,17 @@ CREATE DATABASE easymatch_transport;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    phone VARCHAR(20),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    phone VARCHAR(50),
+    email VARCHAR(500) UNIQUE,
+    password VARCHAR(500),
     date_naissance DATE,
-    post CHECK (post in ('admin', 'expediteur', 'conducteur')),
-    matricule VARCHAR(50) UNIQUE,
+    post VARCHAR CHECK (post in ('admin', 'expediteur', 'conducteur')),
+    matricule VARCHAR(100) UNIQUE,
     pays VARCHAR(100),
     ville VARCHAR(100),
-    statut VARCHAR(50),
+    statut VARCHAR CHECK (statut in ('accepted', 'blocked', 'pending')),
     date_bloque DATE
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE users (
 CREATE TABLE verify_badge (
     id SERIAL PRIMARY KEY,
     conducteur INT REFERENCES users(id) ON DELETE CASCADE,
-    badge_verifier CHECK (badge_verifier in ('oui', 'non')),
+    badge_verifier VARCHAR CHECK (badge_verifier in ('oui', 'non')),
     nombre_etoile INT
 );
 
@@ -46,37 +46,37 @@ CREATE TABLE notification (
 
 CREATE TABLE avis (
     id SERIAL PRIMARY KEY,
-    expediteur_id INT REFERENCES expediteur(user_id) ON DELETE CASCADE,
-    conducteur_id INT REFERENCES conducteur(user_id) ON DELETE CASCADE,
+    expediteur INT REFERENCES users(id) ON DELETE CASCADE,
+    conducteur INT REFERENCES users(id) ON DELETE CASCADE,
     message_conducteur TEXT,
     message_expediteur TEXT
 );
 
 CREATE TABLE trajet (
     id SERIAL PRIMARY KEY,
-    conducteur_id INT REFERENCES conducteur(user_id) ON DELETE CASCADE,
-    point_depart VARCHAR(255),
-    point_destination VARCHAR(255),
-    trajet_itineraire TEXT[],
-    type_vehicule VARCHAR(100),
+    conducteur_id INT REFERENCES users(id) ON DELETE CASCADE,
+    point_depart VARCHAR(100),
+    point_destination VARCHAR(100),
     date_offre DATE,
     date_limite_offre DATE,
-    fragile_admit CHECK (fragile_admit in ('oui', 'non')),        
-    matricule_vehicule VARCHAR(50),
+    trajet_itineraire TEXT,
+    type_vehicule VARCHAR(100),
+    fragile_admit VARCHAR CHECK (fragile_admit in ('oui', 'non')),        
+    matricule_vehicule VARCHAR(100),
     size_colis VARCHAR(50),
-    package_car TEXT[]
+    package_car TEXT
 );
 
 CREATE TABLE demande_expediteur (
     id SERIAL PRIMARY KEY,
-    expediteur_id INT REFERENCES expediteur(user_id) ON DELETE CASCADE,
-    fragile VARCHAR(10),
+    expediteur_id INT REFERENCES users(id) ON DELETE CASCADE,
+    fragile VARCHAR CHECK (fragile in ('oui', 'non')) DEFAULT 'non',
     date_reservation DATE
 );
 
-CREATE TABLE fragile_colier (
+CREATE TABLE fragile_colier_reservé (
     id SERIAL PRIMARY KEY,
     demande_id INT REFERENCES demande_expediteur(id) ON DELETE CASCADE,
-    size_colier TEXT[],
+    size_colier TEXT,
     nbr_colier_fragile INT
 );
